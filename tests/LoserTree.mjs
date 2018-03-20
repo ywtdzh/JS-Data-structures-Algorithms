0c092a4ca0fd740b61c89ec6796b222993691455
Claim <ywtyx01@126.com>
2018-03-20 08:14:18 +0800

    fix test

--- tests/LoserTree.mjs
+++ tests/LoserTree.mjs
@@ -2,14 +2,19 @@ import LoserTree from '../LoserTree.mjs';
 
 const messages = [];
 
-const data = [];
-data.length = 300;
-data.forEach((d, index) => (data[index] = Math.random() * 1001 - 500));
+const data = [[], [], [], [], []];
+data.forEach(array => {
+    let base = -75;
+    array.length = 300;
+    array.forEach((e,index)=>{
+        array[index] = base += Math.random();
+    });
+});
 
-const minLoserTree = new LoserTree(data);
+const minLoserTree = new LoserTree(...data);
 let minValue = minLoserTree.getWinner();
 
-for (let i = 0; i < data.length - 1; i++) {
+for (let i = 0; i < 1499 - 1; i++) {
     let newValue = minLoserTree.getWinner();
     if (minValue > newValue) {
         messages.push(`Test Failed：${newValue} >= ${minValue} should be false!`);
